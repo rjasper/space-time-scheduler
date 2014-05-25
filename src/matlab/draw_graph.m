5e7a08d752318085577f06ad942dcd363f63e53a
Rico Jasper <jasper.rico@googlemail.com>
2014-05-25 10:42:40 +0200

    draw_graph now shows directions

--- src/matlab/draw_graph.m
+++ src/matlab/draw_graph.m
@@ -11,11 +11,16 @@ for k = 1:N
 %     if i(k) > j(k)
 %         continue
 %     end
+
+    V1 = V(:, ij(1, k));
+    V2 = V(:, ij(2, k));
+    dV = V2 - V1;
     
-    line(V(1, ij(:, k)')', V(2, ij(:, k)')');
-    hold on;
+%     line(V(1, ij(:, k)')', V(2, ij(:, k)')');
+    quiver(V1(1), V1(2), dV(1), dV(2), 0, 'b');
+%     hold on;
 end
 
-if ~was_hold
-    hold off;
-end
+% if ~was_hold
+%     hold off;
+% end
