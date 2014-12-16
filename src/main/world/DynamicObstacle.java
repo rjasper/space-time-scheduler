d9a9e7a002707831854450b568154b0561126844
Rico Jasper <jasper.rico@googlemail.com>
2014-12-16 16:52:39 +0100

    documented Trajectory and DynamicObstacle

--- src/main/world/DynamicObstacle.java
+++ src/main/world/DynamicObstacle.java
@@ -2,65 +2,142 @@ package world;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 
+/**
+ * <p>
+ * A {@code DynamicObstacle} represents a physical object in the real world. It
+ * describes the shape and the trajectory of such an object. The shape's origin
+ * marks the center of the object. Offsetting the shape by a spatial point of
+ * the trajectory gives the position of the shape to given time.
+ * </p>
+ * 
+ * @author Rico
+ */
 public class DynamicObstacle implements Cloneable {
 
+	/**
+	 * The physical shape.
+	 */
 	private Polygon shape;
 
+	/**
+	 * The trajectory.
+	 */
 	private Trajectory trajectory;
 
+	/**
+	 * Creates a new {@code DynamicObstacle} with the given physical shape and
+	 * trajectory movement.
+	 * 
+	 * @param shape
+	 * @param trajectory
+	 * @throws NullPointerException
+	 *             if any argument is {@code null}.
+	 * @throws IllegalArgumentException
+	 *             if any of the following is true:
+	 *             <ul>
+	 *             <li>The shape is empty, non-simple, or invalid.</li>
+	 *             <li>The trajectory is empty.</li>
+	 *             </ul>
+	 */
 	public DynamicObstacle(Polygon shape, Trajectory trajectory) {
-		// TODO check if polygon or trajectory is empty
+		Objects.requireNonNull(shape, "shape");
+		Objects.requireNonNull(trajectory, "trajectory");
+		
+		if (shape.isEmpty() || !shape.isSimple() || !shape.isValid())
+			throw new IllegalArgumentException("illegal shape");
+		if (trajectory.isEmpty())
+			throw new IllegalArgumentException("illegal trajectory");
 
 		this.shape = shape;
 		this.trajectory = trajectory;
 	}
 
+	/**
+	 * @return the physical shape.
+	 */
 	public Polygon getShape() {
 		return shape;
 	}
 
+	/**
+	 * @return the trajectory describing the movement.
+	 */
 	public Trajectory getTrajectory() {
 		return trajectory;
 	}
 
+	/**
+	 * @return the spatial path of the trajectory.
+	 * @see Trajectory#getSpatialPath()
+	 */
 	public List<Point> getSpatialPath() {
-		return trajectory == null
-			? null
-			: trajectory.getSpatialPath();
+		return trajectory.getSpatialPath();
 	}
 
+	/**
+	 * @return the start location of the trajectory.
+	 * @see Trajectory#getStartLocation()
+	 */
 	public Point getStartLocation() {
 		return trajectory.getStartLocation();
 	}
 
+	/**
+	 * @return the finish location of the trajectory.
+	 * @see Trajectory#getFinishLocation()
+	 */
 	public Point getFinishLocation() {
 		return trajectory.getFinishLocation();
 	}
 
+	/**
+	 * @return the time ordinates of the trajectory.
+	 * @see Trajectory#getTimes()
+	 */
 	public List<LocalDateTime> getTimes() {
-		return trajectory == null
-			? Collections.emptyList()
-			: trajectory.getTimes();
+		return trajectory.getTimes();
 	}
 
+	/**
+	 * @return the start time of the trajectory.
+	 * @see Trajectory#getStartTime()
+	 */
 	public LocalDateTime getStartTime() {
 		return trajectory.getStartTime();
 	}
 
+	/**
+	 * @return the finish time of the trajectory
+	 * @see Trajectory#getFinishTime()
+	 */
 	public LocalDateTime getFinishTime() {
 		return trajectory.getFinishTime();
 	}
 
+	/**
+	 * @return the duration of the trajectory.
+	 * @see Trajectory#getDuration()
+	 */
 	public Duration getDuration() {
 		return trajectory.getDuration();
 	}
 
+	/**
+	 * Creates a new {@code DynamicObstacle} with a buffered version of this
+	 * one's shape.
+	 * 
+	 * @param distance
+	 *            of the buffer
+	 * @return the buffered version.
+	 * @throws IllegalArgumentException
+	 *             if {@code distance} is not a positive finite number.
+	 */
 	public DynamicObstacle buffer(double distance) {
 		if (!Double.isFinite(distance) || distance < 0.0)
 			throw new IllegalArgumentException("invalid distance");
--- src/main/world/Trajectory.java
+++ src/main/world/Trajectory.java
@@ -4,37 +4,117 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 
+/**
+ * <p>
+ * A {@code Trajectory} represents a curve in 3-dimensional space with spatial
+ * and temporal ordinates. It describes the position of a point for a given
+ * time. The common use is to describe the movement of objects.
+ * </p>
+ * 
+ * <p>
+ * The curve is given by a list of multiple 3D points. Each point consists of
+ * two spatial ordinates (x and y) and a temporal ordinate (t). The points
+ * represent the vertices of the trajectory and are connected via lines. An
+ * empty trajectory without any points is also possible. However, singular
+ * trajectories consisting of only one vertex are forbidden.
+ * </p>
+ * 
+ * <p>
+ * The spatial ordinates are stored as a list of 2-dimensional {@link Point}s
+ * (see {@link #getSpatialPath()}) while the temporal ordinates are a list of
+ * {@link LocalDateTime}s (see {@link #getTimes()}). The i-th elements of each
+ * list compose the i-th 3D vertex of the trajectory.
+ * </p>
+ * 
+ * <p>
+ * The temporal ordinates are constraint since objects can only move forward in
+ * time. Therefore, the time ordinate must increase from point to point.
+ * </p>
+ * 
+ * @author Rico
+ */
 public interface Trajectory {
 
+	/**
+	 * @return {@code true} iff trajectory has no vertices.
+	 */
 	public abstract boolean isEmpty();
 
+	/**
+	 * @return the spatial ordinates (x-y).
+	 */
 	public abstract List<Point> getSpatialPath();
 
+	/**
+	 * @return the temporal ordinates (t).
+	 */
 	public abstract List<LocalDateTime> getTimes();
 
-	public abstract LocalDateTime getFinishTime();
-
-	public abstract LocalDateTime getStartTime();
-
-	public abstract Point getFinishLocation();
-
+	/**
+	 * @return the location of the first vertex. {@code null} if trajectory is
+	 *         empty.
+	 */
 	public abstract Point getStartLocation();
 
+	/**
+	 * @return the location of the last vertex. {@code null} if trajectory is
+	 *         empty.
+	 */
+	public abstract Point getFinishLocation();
+
+	/**
+	 * @return the time of the first vertex. {@code null} if trajectory is
+	 *         empty.
+	 */
+	public abstract LocalDateTime getStartTime();
+
+	/**
+	 * @return the time of the last vertex. {@code null} if trajectory is empty.
+	 */
+	public abstract LocalDateTime getFinishTime();
+
+	/**
+	 * @return the time difference between the first and last vertex.
+	 */
 	public abstract Duration getDuration();
 
+	/**
+	 * @return the euclidean length of the spatial path.
+	 */
 	public abstract double getLength();
 
+	/**
+	 * @return the trace which is a geometry only including all points of the
+	 *         spatial path.
+	 */
 	public abstract Geometry getTrace();
 
-	public default Trajectory merge(Trajectory rhs) {
+	/**
+	 * Calculates the merge of two trajectories. This trajectory serves as
+	 * first section while the given one as the second one. This trajectory will
+	 * not be modified. The given trajectory must have later time ordinates.
+	 * The resulting trajectory connects the original ones by a straight line.
+	 * 
+	 * @param other the second trajectory section.
+	 * @return the merged trajectory.
+	 * @throws NullPointerException if other is {@code null}.
+	 * @throws IllegalArgumentException if other's time is before this one's.
+	 */
+	public default Trajectory merge(Trajectory other) {
+		Objects.requireNonNull(other, "other");
+		
+		if (getFinishTime().compareTo(other.getStartTime()) > 0)
+			throw new IllegalArgumentException("other is before this one");
+		
 		List<Point> lhsSpatialPath = getSpatialPath();
-		List<Point> rhsSpatialPath = rhs.getSpatialPath();
+		List<Point> rhsSpatialPath = other.getSpatialPath();
 		List<LocalDateTime> lhsTimes = getTimes();
-		List<LocalDateTime> rhsTimes = rhs.getTimes();
+		List<LocalDateTime> rhsTimes = other.getTimes();
 
 		int n = lhsSpatialPath.size() + rhsSpatialPath.size();
 
@@ -48,6 +128,6 @@ public interface Trajectory {
 		times.addAll(rhsTimes);
 
 		return new SimpleTrajectory(spatialPath, times);
-	};
+	}
 
 }
