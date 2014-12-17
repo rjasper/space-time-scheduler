001d83787057bbd054131e3f5fe8da138fbcabf9
Rico Jasper <jasper.rico@googlemail.com>
2014-12-17 13:55:33 +0100

    documented ArraysClone

--- src/main/util/ArraysClone.java
+++ src/main/util/ArraysClone.java
@@ -5,10 +5,22 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+/**
+ * Provides an array deep clone copy operation.
+ * 
+ * @author Rico
+ */
 public final class ArraysClone {
 	
 	private ArraysClone() {}
 	
+	/**
+	 * Creates a new array containing a clone of each element. If an element is
+	 * an array itself, then a new array is created recursively.
+	 * 
+	 * @param array to be copied
+	 * @return the copy.
+	 */
 	public static <T extends Cloneable> T[] deepCloneCopy(T[] array) {
 		int n = array.length;
 		Class<?> clazz = array.getClass();
@@ -22,6 +34,13 @@ public final class ArraysClone {
 		return clone;
 	}
 	
+	/**
+	 * Clones the given element. If the element is an array, then a new array
+	 * is created containing clones of the original elements.
+	 * 
+	 * @param element to be cloned
+	 * @return the clone.
+	 */
 	@SuppressWarnings("unchecked")
 	private static <T extends Cloneable> T cloneElement(T element) {
 		if (element == null)
@@ -29,6 +48,7 @@ public final class ArraysClone {
 		
 		Class<?> clazz = element.getClass();
 		
+		// if array then make an element-wise clone copy
 		if (clazz.isArray()) {
 			Class<?> componentType = clazz.getComponentType();
 			
@@ -37,35 +57,39 @@ public final class ArraysClone {
 				return (T) deepCloneCopy((Cloneable[]) element);
 			} else {
 				int n = Array.getLength(element);
+
+				// use standard copy operation
 				
 				if (componentType.isPrimitive()) {
-					if (componentType == boolean.class)
+					if      (componentType == boolean.class)
 						return (T) Arrays.copyOf((boolean[]) element, n);
-					else if (componentType == char.class)
-						return (T) Arrays.copyOf((char[]) element, n);
-					else if (componentType == byte.class)
-						return (T) Arrays.copyOf((byte[]) element, n);
-					else if (componentType == short.class)
-						return (T) Arrays.copyOf((short[]) element, n);
-					else if (componentType == int.class)
-						return (T) Arrays.copyOf((int[]) element, n);
-					else if (componentType == long.class)
-						return (T) Arrays.copyOf((long[]) element, n);
-					else if (componentType == float.class)
-						return (T) Arrays.copyOf((float[]) element, n);
-					else // (componentType == double.class)
-						return (T) Arrays.copyOf((double[]) element, n);
+					else if (componentType == char   .class)
+						return (T) Arrays.copyOf((char   []) element, n);
+					else if (componentType == byte   .class)
+						return (T) Arrays.copyOf((byte   []) element, n);
+					else if (componentType == short  .class)
+						return (T) Arrays.copyOf((short  []) element, n);
+					else if (componentType == int    .class)
+						return (T) Arrays.copyOf((int    []) element, n);
+					else if (componentType == long   .class)
+						return (T) Arrays.copyOf((long   []) element, n);
+					else if (componentType == float  .class)
+						return (T) Arrays.copyOf((float  []) element, n);
+					else // (componentType == double .class)
+						return (T) Arrays.copyOf((double []) element, n);
 				} else {
 					return (T) Arrays.copyOf((Object[]) element, n);
 				}
 			}
+		// if no array try to clone
 		} else {
 			try {
 				Method cloneMethod = clazz.getMethod("clone");
 				
 				return (T) cloneMethod.invoke(element);
 			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				throw new RuntimeException(e);
+				// if element is not publicly cloneable then return element itself
+				return element;
 			}
 		}
 	}
