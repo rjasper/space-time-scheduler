200e009c9f6c4f4c3cb425d42611fa673c7916a7
Rico Jasper <jasper.rico@googlemail.com>
2015-01-20 17:05:01 +0100

    documented GeometryIterator

--- src/main/jts/geom/util/GeometryIterator.java
+++ src/main/jts/geom/util/GeometryIterator.java
@@ -11,16 +11,43 @@ import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.Polygon;
 
 // TODO implement test
+/**
+ * Iterates over the contained geometries of a geometry.
+ * 
+ * @author Rico
+ */
 public class GeometryIterator implements Iterator<Geometry> {
 	
+	/**
+	 * The stack of geometry iterators.
+	 */
 	private final Stack<Iterator<Geometry>> stack = new Stack<>();
 	
+	/**
+	 * Whether to iterate over the components of a {@link Polygon} rather than
+	 * the {@code Polygon} itself.
+	 */
 	private final boolean overComponents;
 	
+	/**
+	 * Constructs a new {@code GeometryIterator} for the given {@code Geometry}.
+	 * Does not iterate over the components of a {@code Polygon} but over the
+	 * {@code Polygon} itself.
+	 * 
+	 * @param geometry
+	 */
 	public GeometryIterator(Geometry geometry) {
 		this(geometry, false);
 	}
-	
+
+	/**
+	 * Constructs a new {@code GeometryIterator} for the given {@code Geometry}.
+	 * 
+	 * @param geometry
+	 * @param overComponents
+	 *            whether to iterate over the components of a {@link Polygon}
+	 *            rather than the {@code Polygon} itself
+	 */
 	public GeometryIterator(Geometry geometry, boolean overComponents) {
 		Objects.requireNonNull(geometry, "geometry");
 		
@@ -29,11 +56,19 @@ public class GeometryIterator implements Iterator<Geometry> {
 		stack.push(makeIterator(geometry));
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Iterator#hasNext()
+	 */
 	@Override
 	public boolean hasNext() {
 		return !stack.isEmpty();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Iterator#next()
+	 */
 	@Override
 	public Geometry next() {
 		// breaks by returning a non geometry collection
@@ -66,32 +101,73 @@ public class GeometryIterator implements Iterator<Geometry> {
 		}
 	}
 	
+	/**
+	 * Makes a non-recursive {@code Iterator} for a geometry.
+	 * 
+	 * @param geometry
+	 * @return the iterator.
+	 */
 	private static Iterator<Geometry> makeIterator(Geometry geometry) {
 		return new SubGeometryIterator(geometry);
 	}
 	
+	/**
+	 * Helper {@code Iterator} class to iterate over the geometry components of
+	 * a geometry. Does not recursively work on components of components.
+	 * 
+	 * @author Rico
+	 */
 	private static class SubGeometryIterator implements Iterator<Geometry> {
+		
+		/**
+		 * The geometry to iterate over.
+		 */
 		private final Geometry geometry;
 		
+		/**
+		 * The position of the current component.
+		 */
 		private int i = 0;
 
+		/**
+		 * Constructs a new non-recursive geometry component iterator of the
+		 * given geometry.
+		 * 
+		 * @param geometry
+		 */
 		public SubGeometryIterator(Geometry geometry) {
 			this.geometry = geometry;
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#hasNext()
+		 */
 		@Override
 		public boolean hasNext() {
 			return i < geometry.getNumGeometries();
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#next()
+		 */
 		@Override
 		public Geometry next() {
 			return geometry.getGeometryN(i++);
 		}
 	}
 	
+	/**
+	 * Special {@code Iterator} to iterate over the components of a {@link Polygon}
+	 * 
+	 * @author Rico
+	 */
 	private static class PolygonComponentIterator implements Iterator<Geometry> {
 		
+		/**
+		 * The polygon to iterate over.
+		 */
 		private final Polygon polygon;
 		
 		/**
@@ -99,18 +175,35 @@ public class GeometryIterator implements Iterator<Geometry> {
 		 */
 		private boolean exterior;
 		
+		/**
+		 * The position of the current interior component to be iterated over.
+		 */
 		private int interior = 0;
 
+		/**
+		 * Constructs a new {@code PolygonComponentIterator} of the given
+		 * {@code Polygon}.
+		 * 
+		 * @param polygon
+		 */
 		public PolygonComponentIterator(Polygon polygon) {
 			this.polygon = polygon;
 			this.exterior = !polygon.isEmpty();
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#hasNext()
+		 */
 		@Override
 		public boolean hasNext() {
 			return exterior || interior < polygon.getNumInteriorRing();
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * @see java.util.Iterator#next()
+		 */
 		@Override
 		public Geometry next() {
 			if (exterior) {
