7293b5be35aeef1b5371b875f8a223e00e3dab21
Rico Jasper <jasper.rico@googlemail.com>
2015-03-16 11:43:58 +0100

    implemented descending iterator test

--- src/main/de/tu_berlin/mailbox/rjasper/st_scheduler/scheduler/util/MappedIntervalSet.java
+++ src/main/de/tu_berlin/mailbox/rjasper/st_scheduler/scheduler/util/MappedIntervalSet.java
@@ -14,9 +14,9 @@ import java.util.function.Function;
 public class MappedIntervalSet<T extends Comparable<? super T>, U>
 extends AbstractIntervalSet<T>
 {
-	
+
 	private final NavigableMap<T, U> map;
-	
+
 	private final Function<U, Interval<T>> intervalMapper;
 
 	public MappedIntervalSet(
@@ -26,17 +26,17 @@ extends AbstractIntervalSet<T>
 		this.map = Objects.requireNonNull(map, "map");
 		this.intervalMapper = Objects.requireNonNull(intervalMapper, "intervalMapper");
 	}
-	
+
 	private Interval<T> interval(Entry<T, U> entry) {
 		// TODO cache?
 		return entry == null ? null : intervalMapper.apply(entry.getValue());
 	}
-	
+
 	private Interval<T> interval(U obj) {
 		// TODO cache?
 		return obj == null ? null : intervalMapper.apply(obj);
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see scheduler.util.IntervalSet#isEmpty()
 	 */
@@ -44,7 +44,7 @@ extends AbstractIntervalSet<T>
 	public boolean isEmpty() {
 		return map.isEmpty();
 	}
-	
+
 	@Override
 	public de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.util.IntervalSet.Interval<T> minInterval() {
 		return interval( map.firstEntry() );
@@ -79,17 +79,17 @@ extends AbstractIntervalSet<T>
 	public IntervalSet<T> subSet(T fromInclusive, T toExclusive) {
 		return new SubSet(fromInclusive, toExclusive);
 	}
-	
+
 	private class SubSet extends MappedIntervalSet<T, U> {
-		
+
 		public SubSet(T fromInclusive, T toExclusive) {
 			super(
 				makeSubMap(MappedIntervalSet.this, fromInclusive, toExclusive),
 				MappedIntervalSet.this.intervalMapper);
 		}
-		
+
 	}
-	
+
 	private static <U, T extends Comparable<? super T>>
 	NavigableMap<T, U> makeSubMap(
 		MappedIntervalSet<T, U> self,
@@ -97,83 +97,83 @@ extends AbstractIntervalSet<T>
 	{
 		Interval<T> lowerInterval = self.lowerInterval(fromInclusive);
 		T from;
-		
+
 		if (lowerInterval != null && lowerInterval.getToExclusive().compareTo(fromInclusive) > 0)
 			from = lowerInterval.getFromInclusive();
 		else
 			from = fromInclusive;
-		
+
 		return (NavigableMap<T, U>) self.map.subMap(from, toExclusive);
 	}
 
 	private static class IntervalIterator<T extends Comparable<? super T>>
 	implements Iterator<Interval<T>>
 	{
-		
+
 		private final Iterator<Interval<T>> iterator;
-		
+
 		private final boolean descending;
-		
+
 		private Interval<T> peek = null;
-		
+
 		public IntervalIterator(Iterator<Interval<T>> iterator, boolean descending) {
 			this.iterator = Objects.requireNonNull(iterator, "iterator");
 			this.descending = descending;
 			this.peek = iterator.hasNext() ? iterator.next() : null;
 		}
-	
+
 		@Override
 		public boolean hasNext() {
 			return peek != null;
 		}
-	
+
 		@Override
 		public Interval<T> next() {
 			if (peek == null)
 				throw new NoSuchElementException("no next element");
-			
+
 			Interval<T> first = peek;
 			Interval<T> last = first;
-			
+
 			// seek last non-consecutive interval
 			boolean noBreak = true;
 			if (!descending) {
 				while (iterator.hasNext()) {
 					peek = iterator.next();
-					
+
 					T lastTo = last.getToExclusive();
 					T peekFrom = peek.getFromInclusive();
-					
+
 					if (!peekFrom.equals(lastTo)) {
 						noBreak = false;
 						break;
 					}
-					
+
 					last = peek;
 				}
 			} else {
 				while (iterator.hasNext()) {
 					peek = iterator.next();
-					
+
 					T lastFrom = last.getFromInclusive();
 					T peekTo = peek.getToExclusive();
-					
+
 					if (!peekTo.equals(lastFrom)) {
 						noBreak = false;
 						break;
 					}
-					
+
 					last = peek;
 				}
 			}
-			
+
 			// !iterator.hasNext() || peek.from != last.to
-			
+
 			// if while-loop finished ordinarily
 			// indicates that there are no more intervals to iterate over
 			if (noBreak)
 				peek = null;
-			
+
 			if (last == first) {
 				return first; // reuse interval
 			} else {
@@ -188,25 +188,24 @@ extends AbstractIntervalSet<T>
 				}
 			}
 		}
-		
+
 	}
-	
+
 	@Override
 	public Iterator<Interval<T>> iterator() {
 		Iterator<Interval<T>> it = map.values().stream()
 			.map(this::interval)
 			.iterator();
-		
+
 		return new IntervalIterator<>(it, false);
 	}
 
-	// TODO test
 	@Override
 	public Iterator<Interval<T>> descendingIterator() {
 		Iterator<Interval<T>> it = map.descendingMap().values().stream()
 			.map(this::interval)
 			.iterator();
-		
+
 		return new IntervalIterator<>(it, true);
 	}
 
@@ -214,5 +213,5 @@ extends AbstractIntervalSet<T>
 	public Spliterator<Interval<T>> spliterator() {
 		return Spliterators.spliteratorUnknownSize(iterator(), DISTINCT | NONNULL | ORDERED | SORTED);
 	}
-	
+
 }
--- src/test/de/tu_berlin/mailbox/rjasper/st_scheduler/scheduler/util/MappedIntervalSetTest.java
+++ src/test/de/tu_berlin/mailbox/rjasper/st_scheduler/scheduler/util/MappedIntervalSetTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.TreeMap;
 import java.util.UUID;
 
@@ -20,44 +21,48 @@ import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.util.IntervalSet.Inte
 import de.tu_berlin.mailbox.rjasper.util.UUIDFactory;
 
 public class MappedIntervalSetTest {
-	
+
 	private static Job job(LocalDateTime startTime, LocalDateTime finishTime) {
 		UUID id = UUIDFactory.uuid("don't care");
 		Node node = NodeFactory.getInstance()
 			.createNode("don't care", 0, 0);
 		ImmutablePoint location = StaticGeometryBuilder.immutablePoint(0, 0);
 		Duration duration = Duration.between(startTime, finishTime);
-		
+
 		return new Job(id, node.getReference(), location, startTime, duration);
 	}
-	
+
 	private static Interval<LocalDateTime> interval(Job job) {
 		return new Interval<>(job.getStartTime(), job.getFinishTime());
 	}
-	
+
 	private static IntervalSet<LocalDateTime> mappedIntervalSet(LocalDateTime... times) {
 		TreeMap<LocalDateTime, Job> jobMap = new TreeMap<>();
 
 		for (int i = 0; i < times.length;) {
 			LocalDateTime start = times[i++];
 			LocalDateTime finish = times[i++];
-			
+
 			jobMap.put(start, job(start, finish));
 		}
-		
+
 		return new MappedIntervalSet<>(jobMap, MappedIntervalSetTest::interval);
 	}
-	
+
 	private static IntervalSet<LocalDateTime> simpleIntervalSet(LocalDateTime... times) {
 		if (times.length % 2 != 0)
 			throw new IllegalArgumentException("invalid number of times");
-		
+
 		SimpleIntervalSet<LocalDateTime> set = new SimpleIntervalSet<>();
-		
+
 		for (int i = 0; i < times.length;)
 			set.add(times[i++], times[i++]);
-		
+
 		return set;
+	}
+
+	private static Interval<LocalDateTime> interval(LocalDateTime from, LocalDateTime to) {
+		return new Interval<>(from, to);
 	}
 
 	@Test
@@ -65,58 +70,86 @@ public class MappedIntervalSetTest {
 		IntervalSet<LocalDateTime> jobSet = mappedIntervalSet(
 			atSecond(0), atSecond(1),
 			atSecond(4), atSecond(5));
-		
+
 		IntervalSet<LocalDateTime> timeSet = simpleIntervalSet(
 			atSecond(2), atSecond(3),
 			atSecond(6), atSecond(7));
-		
+
 		IntervalSet<LocalDateTime> res = jobSet.union(timeSet);
-		
+
 		IntervalSet<LocalDateTime> expected = simpleIntervalSet(
 			atSecond(0), atSecond(1),
 			atSecond(2), atSecond(3),
 			atSecond(4), atSecond(5),
 			atSecond(6), atSecond(7));
-		
+
 		assertThat(res, equalTo(expected));
 	}
-	
+
 	@Test
 	public void testDifferenceSet() {
 		IntervalSet<LocalDateTime> jobSet = mappedIntervalSet(
 			atSecond(0), atSecond(1),
 			atSecond(3), atSecond(5));
-		
+
 		IntervalSet<LocalDateTime> timeSet = simpleIntervalSet(
 			atSecond(1), atSecond(4),
 			atSecond(6), atSecond(7));
-		
+
 		IntervalSet<LocalDateTime> res = jobSet.difference(timeSet);
-		
+
 		IntervalSet<LocalDateTime> expected = simpleIntervalSet(
 			atSecond(0), atSecond(1),
 			atSecond(4), atSecond(5));
-		
+
 		assertThat(res, equalTo(expected));
 	}
-	
+
 	@Test
 	public void testIntersectionSet() {
 		IntervalSet<LocalDateTime> jobSet = mappedIntervalSet(
 			atSecond(0), atSecond(2),
 			atSecond(3), atSecond(5));
-		
+
 		IntervalSet<LocalDateTime> timeSet = simpleIntervalSet(
 			atSecond(1), atSecond(4),
 			atSecond(6), atSecond(7));
-		
+
 		IntervalSet<LocalDateTime> res = jobSet.intersection(timeSet);
-		
+
 		IntervalSet<LocalDateTime> expected = simpleIntervalSet(
 			atSecond(1), atSecond(2),
 			atSecond(3), atSecond(4));
-		
+
 		assertThat(res, equalTo(expected));
+	}
+
+	@Test
+	public void testIterator() {
+		IntervalSet<LocalDateTime> jobSet = mappedIntervalSet(
+			atSecond(0), atSecond(2),
+			atSecond(2), atSecond(4),
+			atSecond(5), atSecond(6));
+
+		Iterator<Interval<LocalDateTime>> it = jobSet.iterator();
+
+		assertThat(it.next(), equalTo(interval(atSecond(0), atSecond(4))));
+		assertThat(it.next(), equalTo(interval(atSecond(5), atSecond(6))));
+		assertThat(it.hasNext(), is(false));
+	}
+
+	@Test
+	public void testDescendingIterator() {
+		IntervalSet<LocalDateTime> jobSet = mappedIntervalSet(
+			atSecond(0), atSecond(2),
+			atSecond(2), atSecond(4),
+			atSecond(5), atSecond(6));
+
+		Iterator<Interval<LocalDateTime>> it = jobSet.descendingIterator();
+
+		assertThat(it.next(), equalTo(interval(atSecond(5), atSecond(6))));
+		assertThat(it.next(), equalTo(interval(atSecond(0), atSecond(4))));
+		assertThat(it.hasNext(), is(false));
 	}
 
 }
