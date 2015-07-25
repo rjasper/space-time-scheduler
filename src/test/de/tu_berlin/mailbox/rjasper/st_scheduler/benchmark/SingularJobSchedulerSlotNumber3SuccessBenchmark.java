05099e92485efabd458eb610f2c129cbcbfbbf24
Rico Jasper <jasper.rico@googlemail.com>
2015-07-25 16:52:09 +0200

    implemented new benchmark scenario

---
+++ src/test/de/tu_berlin/mailbox/rjasper/st_scheduler/benchmark/SingularJobSchedulerSlotNumber3SuccessBenchmark.java
@@ -0,0 +1,122 @@
+package de.tu_berlin.mailbox.rjasper.st_scheduler.benchmark;
+
+import static de.tu_berlin.mailbox.rjasper.jts.geom.immutable.StaticGeometryBuilder.*;
+import static de.tu_berlin.mailbox.rjasper.st_scheduler.world.factories.TrajectoryFactory.*;
+import static de.tu_berlin.mailbox.rjasper.time.TimeConv.*;
+import static de.tu_berlin.mailbox.rjasper.time.TimeFactory.*;
+import static de.tu_berlin.mailbox.rjasper.util.UUIDFactory.*;
+
+import java.time.Duration;
+
+import de.tu_berlin.mailbox.rjasper.jts.geom.immutable.ImmutablePoint;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.CollisionException;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.Job;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.JobSpecification;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.Node;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.NodeReference;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.NodeSpecification;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.ScheduleResult;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.scheduler.Scheduler;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.world.Trajectory;
+import de.tu_berlin.mailbox.rjasper.st_scheduler.world.World;
+
+public class SingularJobSchedulerSlotNumber3SuccessBenchmark implements Benchmarkable {
+
+	private StopWatch sw = new StopWatch();
+
+	@Override
+	public int minProblemSize() {
+		return 100;
+	}
+
+	@Override
+	public int maxProblemSize() {
+		return 1000;
+	}
+
+	@Override
+	public int stepProblemSize() {
+		return 100;
+	}
+
+	@Override
+	public Duration benchmark(int n) {
+		sw.reset();
+
+		Scheduler sc = new Scheduler(new World());
+
+		addNodes(sc, n);
+
+		JobSpecification spec = JobSpecification.createSS(
+			uuid("job"),
+			immutablePoint(3, 0),
+			atSecond(1), atSecond(10*(n-1)-1),
+			secondsToDurationSafe(1));
+
+		sw.start();
+		ScheduleResult res = sc.schedule(spec);
+		sw.stop();
+
+		if (res.isError())
+			throw new AssertionError("job was not scheduled");
+
+//		System.out.println(res.getJobs().get(uuid("job")));
+
+		return sw.duration();
+	}
+
+	public void addNodes(Scheduler sc, int n) {
+		NodeSpecification spec1 = new NodeSpecification(
+			"node1",
+			immutableBox(-1, -1, 1, 1),
+			1.0,
+			immutablePoint(0, 0),
+			atSecond(0));
+
+		NodeSpecification spec2 = new NodeSpecification(
+			"node2",
+			immutableBox(-1, -1, 1, 1),
+			1.0,
+			immutablePoint(6, 0),
+			atSecond(0));
+
+		try {
+			sc.addNode(spec1);
+			sc.addNode(spec2);
+		} catch (CollisionException e) {
+			e.printStackTrace();
+		}
+
+		Node node1 = sc.__debug__getNode("node1");
+		NodeReference ref1 = node1.getReference();
+		ImmutablePoint location1 = node1.getInitialLocation();
+
+		Node node2 = sc.__debug__getNode("node2");
+		NodeReference ref2 = node2.getReference();
+		ImmutablePoint location2 = node2.getInitialLocation();
+
+		for (int i = 0; i < n; ++i) {
+			Job job1 = new Job(
+				uuid("job1#" + Integer.toString(i)),
+				ref1,
+				location1,
+				atSecond(10*i),
+				secondsToDurationSafe(1));
+			Job job2 = new Job(
+				uuid("job2#" + Integer.toString(i)),
+				ref2,
+				location2,
+				atSecond(10*i),
+				secondsToDurationSafe(1));
+			Trajectory traj1 = trajectory(0, 0, 0, 0, 10*i, 10*i+1);
+			Trajectory traj2 = trajectory(3, 3, 0, 0, 10*i, 10*i+1);
+
+			node1.addJob(job1);
+			node1.updateTrajectory(traj1);
+
+			node2.addJob(job2);
+			node2.updateTrajectory(traj2);
+		}
+	}
+
+}
